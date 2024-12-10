ec73195859b3329e54e10c5a7424fcd90af1fcfd
rupalbhure <rupalkbhure21@gmail.com>
2024-12-10 16:55:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let myImgEl = document.getElementById("myImg");
+
+function onSpringBtn(){
+
+    myImgEl.src = "spring.jpg";
+    myImgEl.style.height = "500px";
+    
+}
+
+function onSummerBtn(){
+
+    myImgEl.src = "summer.jpg";
+    myImgEl.style.height = "500px";
+
+}
+
+function onAutumnBtn(){
+
+    myImgEl.src = "Autum.jpg";
+    myImgEl.style.height = "500px";
+
+}
+
+function onWinterBtn(){
+
+    myImgEl.src = "winter.jpg";
+    myImgEl.style.height = "500px";
+    
+}
